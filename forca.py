3a4f95c9b8ca99dda11125009d05ea6d1ced22c8
Leonardo <leomaltadesa0712@gmail.com>
2023-08-07 23:43:27 -0300

    feat: initial commit

--- forca.py
+++ forca.py
@@ -8,5 +8,23 @@ def jogar():
 
     print("Fim do jogo")
 
-if(__name__ == 'main'):
+    palavra_secreta = "cerveja"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input('Qual letra?').lower()
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print('Encontrei a letra {} na posicao{}'.format(letra, index))
+            index = index + 1
+
+
+
+
+if(__name__ == "__main__"):
     jogar()
